c382bb33242261cb6e00c9ef03ecf1458c428ec0
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2025-11-25 13:30:03 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('My Flutter App')),
+        body: SafeArea(
+          child: Builder(
+            builder: (context) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.red,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.red,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.deepPurpleAccent,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.deepPurpleAccent,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.tealAccent,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                      Container(
+                        width: 60,
+                        height: 50,
+                        color: Colors.tealAccent,
+                        margin: EdgeInsets.all(10),
+                        child: Text('Hello, World!'),
+                      ),
+                    ],
+                  ),
+                  // ElevatedButton(
+                  //   onPressed: () {
+                  //     print('Click!');
+                  //   },
+                  //   child: const Text('A button'),
+                  // ),
+                ],
+              );
+            },
+          ),
+        ),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+    );
+  }
+}
