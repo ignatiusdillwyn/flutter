946c26905ab948c9105e845f431b2d4966cedfa4
ignatius_dillwyn <v.ignatius.dillwyn@adira.co.id>
2025-12-02 15:37:37 +0700

    add text controller

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() {
   runApp(MainApp());
@@ -6,32 +7,71 @@ void main() {
 
 class MainApp extends StatefulWidget {
   @override
-  State<MainApp> createState() => MyCounterState();
+  State<MainApp> createState() => _MainAppState();
 }
 
-class MyCounterState extends State<MainApp> {
+class _MainAppState extends State<MainApp> {
   int count = 0;
+  List<dynamic> listNama = []; // List untuk menyimpan nama
+  TextEditingController textController =
+      TextEditingController(); // Controller untuk TextField
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Counter App'),
-        ),
-        body: SingleChildScrollView( // Membuat konten scrollable
-          child: Column(
-            children: [
-              Text('Count: $count'),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    count++;
-                  });
-                },
-                child: Text('Increment'),
-              ),
-            ],
+        appBar: AppBar(title: Text('Counter App')),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text('Count: $count'),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      count++;
+                      print(count);
+                    });
+                  },
+                  child: Text('Increment'),
+                ),
+                TextField(
+                  controller:
+                      textController, // Hubungkan controller ke TextField
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter a name',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      if (textController.text.isNotEmpty) {
+                        listNama.add(
+                          textController.text,
+                        ); // Tambahkan teks ke list
+                        textController
+                            .clear(); // Kosongkan TextField setelah menambahkan
+                      } else {
+                        print('INPUT NAME WOI');
+                        Fluttertoast.showToast(
+                          msg: "Please Input Name",
+                          toastLength: Toast.LENGTH_SHORT,
+                          gravity: ToastGravity.BOTTOM,
+                          backgroundColor: Colors.black,
+                          textColor: Colors.white,
+                        );
+                      }
+                    });
+                  },
+                  child: Text('Add to List'),
+                ),
+                SizedBox(height: 20),
+                Text('List of Names:'),
+                for (int i = 0; i < listNama.length; i++)
+                  Text('${i + 1}. ${listNama[i]}'), // Tampilkan dengan nomor
+              ],
+            ),
           ),
         ),
       ),
